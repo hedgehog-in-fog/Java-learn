ab0353bf5e987901d98ad349d592b7bc7fe757bf
hedgehog-in-fog <m.tumanova97@gmail.com>
2020-06-29 17:25:43 +0300

    чтение и запись в файл 3

--- src/java/FileRW/FileRW.java
+++ src/java/FileRW/FileRW.java
@@ -1,45 +1,38 @@
 package FileRW;
+
 import java.io.*;
-import java.math.BigInteger;
+import java.util.Scanner;
 
 public class FileRW {
 	static File file = new File("C:\\Users\\stalnoy\\Desktop\\fileRW.txt");
-	static int sum;
+	static int sum = 0;
 
 	public static void main(String[] args) throws IOException {
 
 		FileWriter fw = new FileWriter(file);
-		fw.write("-72397654 -1");
+		fw.write("4 12 1 78 -14");
 		fw.close();
 		filezRW();
 	}
 
-	private static void filezRW() throws IOException {
-
-		try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-			int i;
-			while ((i = bufferedReader.read()) != Integer.parseInt(" ")){
-				sum += i;
-				bufferedReader.mark(11);
+	private static void filezRW() {
+		try (Scanner scanner = new Scanner(new FileReader(file))) {
+			while (scanner.hasNextInt()) {
+				sum += scanner.nextInt();
 			}
-		}
-
-		try (FileReader fr = new FileReader(file)) {
-
-			char[] buf = new char[23];
-			StringBuilder stringBuilder = new StringBuilder();
-			fr.read(buf);
-			stringBuilder.append(buf);
-			int r1 = Integer.parseInt(stringBuilder.substring(stringBuilder.indexOf(" ") + 1).replaceAll("\\u0000", ""));
-			int r2 = Integer.parseInt(stringBuilder.substring(0, stringBuilder.indexOf(" ")));
-			sum = r1 + r2;
-		} catch (Exception e) {
-
+			System.out.println(sum);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		try (FileWriter fw = new FileWriter(file, true)) {
 			fw.write("\n" + sum);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
 	}
+
 }
+
+
