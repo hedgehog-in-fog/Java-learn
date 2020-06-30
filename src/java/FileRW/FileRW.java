6fb8405ba7281dc4488b00626ba869cfc1d3e8b0
hedgehog-in-fog <m.tumanova97@gmail.com>
2020-06-30 12:38:44 +0300

    чтение и запись в файл 3

--- src/java/FileRW/FileRW.java
+++ src/java/FileRW/FileRW.java
@@ -6,21 +6,49 @@ import java.util.Scanner;
 public class FileRW {
 	static File file = new File("C:\\Users\\stalnoy\\Desktop\\fileRW.txt");
 
-	public static void main(String[] args) throws IOException {
-		test1(" 45 68 99");
-		filezRW();
+	public static void main(String[] args) throws Exception {
+		test1();
+		test2();
 	}
-	public static void test1(String s) throws IOException {
+
+	public static void test1() throws Exception {
+		String s = "3 7 8 -15 127";
 		FileWriter fw = new FileWriter(file);
 		fw.write(s);
 		fw.close();
+		filezRW();
+		Scanner scanner = new Scanner(file);
+		String result = " ";
+		while (scanner.hasNextLine()) {
+			result = result + " " + scanner.nextLine();
+		}
+		scanner.close();
+		if ("  3 7 8 -15 127 130".equals(result)) {
+			System.out.println("test 1 - true");
+		}
 	}
 
-	private static void filezRW() {
+	public static void test2() throws Exception {
+		FileWriter fw = new FileWriter(file);
+		String s = "1 5.9 45";
+		fw.write(s);
+		fw.close();
+		try {
+			filezRW();
+		} catch (IllegalStateException e) {
+			System.out.println("test 2 - true");
+			e.printStackTrace();
+		}
+	}
+
+	private static void filezRW() throws Exception {
 		int sum = 0;
 		try (Scanner scanner = new Scanner(new FileReader(file))) {
 			while (scanner.hasNextInt()) {
 				sum += scanner.nextInt();
+			}
+			if (scanner.hasNext()) {
+				throw new IllegalStateException();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
